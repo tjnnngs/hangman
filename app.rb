d522caa36cddce29cafbaa86f7e9ade436c93622
TONYA JENNINGS <you@example.com>
2017-07-31 14:13:19 -0400

    hangman error

--- app.rb
+++ app.rb
@@ -1,7 +1,29 @@
-def get_word?(secret_word)
-	if secret_word.class == string
+	def get_word?(secret_word) 
+		if secret_word.class == String
 		true
-	else
+		else
 		false
 	end
-	
+end
+
+	number_of_letters_in_secret_word = 0
+	def get_secret_word_length(secret_word)
+		number_of_letters_in_secret_word = secret_word.length
+	end
+
+	def store_word (secret_word)
+		secret_word = "secret_word"
+		letters_in_secret_word = []
+		secret_word.each_char do |letter|
+		letters_in_secret_word.push(letter)
+	end
+end
+
+	def check_letter?(letter)
+		if letters_in_secret_word.include(letter)
+			true
+		else
+			false
+		end
+
+	end
--- app_test.rb
+++ app_test.rb
@@ -2,18 +2,39 @@ require "minitest/autorun"
 require_relative "app.rb"
 
 class TestHangman < Minitest::Test
-	
+
 	def test_assert_that_1_equals_1
 		assert_equal(1, 1)
 	end
 
-	def test_assert_that_empyt_string_returns_true
+	def test_assert_that_empty_string_returns_true
 		secret_word = ""
+		assert_equal(true, get_word?(secret_word)) 
+	end #first value is what we expect, 2nd value is function w/ variable
+
+	def test_assert_string_returns_true
+		secret_word = "cat"
 		assert_equal(true, get_word?(secret_word))
+	end	
+
+	def test_assert_integer_returns_false
+		secret_word = 17
+		assert_equal(false, get_word?(secret_word))
 	end
 
+	def test_assert_array_returns_false
+		secret_word = []
+		assert_equal(false, get_word?(secret_word))
+	end
 
+	def test_assert_thats_length_for_fox_is_3
+		secret_word = "fox"
+		assert_equal(3, get_secret_word_length(secret_word))
+	end
 
-
+	def test_assert_that_f_is_in_array
+		letter = "f"
+		assert_equal(true, check_letter?(letter))
+	end
 
 end
