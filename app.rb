c6140b2e07d06b72e9ed4934d4fad07277bb2a25
TONYA JENNINGS <you@example.com>
2017-08-01 09:50:55 -0400

    hangman file from Monday for mobcoding on Tuesday

--- app.rb
+++ app.rb
@@ -5,9 +5,11 @@ def get_word?(secret_word)
         false
     end
 end
+
 def get_secret_word_length(secret_word)
     number_of_letters_in_secret_word = secret_word.length
 end
+
 def check_letter?(secret_word, letter)
     if secret_word.include? letter
         true
@@ -15,14 +17,17 @@ def check_letter?(secret_word, letter)
         false
     end
 end
+
 def push_correct_guess_into(letter) #this function pushes a correct guess, letter, into an array
     correct_letter = [] 
     correct_letter.push letter
 end
+
 def push_incorrect_guess_into(letter) #see above
     wrong_letter = []
     wrong_letter.push letter
 end
+
 def play_hangman(secret_word, letter) #bringing all the functions from above into this line. Going to have a do loop and a counter. Function in progress.
     if 
         check_letter?(secret_word, letter) == true
@@ -31,4 +36,3 @@ def play_hangman(secret_word, letter) #bringing all the functions from above int
         push_incorrect_guess_into(letter)
     end
 end
-    
--- app_test.rb
+++ app_test.rb
@@ -1,36 +1,55 @@
 require "minitest/autorun"
+
 require_relative "app.rb"
 
 class TestHangman < Minitest::Test
 
-	def test_assert_that_1_equals_1
-		assert_equal(1, 1)
-	end
-
-	def test_assert_that_empty_string_returns_true
-		secret_word = ""
-		assert_equal(true, get_word?(secret_word)) 
-	end #first value is what we expect, 2nd value is function w/ variable
-
-	def test_assert_string_returns_true
-		secret_word = "cat"
-		assert_equal(true, get_word?(secret_word))
-	end	
-
-	def test_assert_integer_returns_false
-		secret_word = 17
-		assert_equal(false, get_word?(secret_word))
-	end
-
-	def test_assert_array_returns_false
-		secret_word = []
-		assert_equal(false, get_word?(secret_word))
-	end
-
-	def test_assert_thats_length_for_fox_is_3
-		secret_word = "fox"
-		assert_equal(3, get_secret_word_length(secret_word))
-	end
-
-
+   
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+   
+    def test_assert_that_empty_string_returns_true
+        secret_word = ""
+        assert_equal(true, get_word?(secret_word)) 
+    end #first value is what we expect, 2nd value is function w/ variable
+   
+    def test_assert_string_returns_true
+        secret_word = "cat"
+        assert_equal(true, get_word?(secret_word))
+    end 
+   
+    def test_assert_integer_returns_false
+        secret_word = 17
+        assert_equal(false, get_word?(secret_word))
+    end
+   
+    def test_assert_array_returns_false
+        secret_word = []
+        assert_equal(false, get_word?(secret_word))
+    end
+   
+    def test_assert_thats_length_for_fox_is_3
+        secret_word = "fox"
+        assert_equal(3, get_secret_word_length(secret_word))
+    end
+   
+    def test_assert_that_f_is_in_the_word
+        secret_word = "fox"
+        letter = "f"
+        assert_equal(true, check_letter?(secret_word, letter))
+    end
+   
+    def test_assert_that_z_is_in_the_word
+        secret_word = "fox"
+        letter = "z"
+        assert_equal(false, check_letter?(secret_word, letter))
+    end
+   
+    def test_assert_f_for_fox_playing_hangman
+        secret_word = "fox"
+        letter = "f"
+        assert_equal(["f"], play_hangman(secret_word, letter))
+    end
+    
 end
